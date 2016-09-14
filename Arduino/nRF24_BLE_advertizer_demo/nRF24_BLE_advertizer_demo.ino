d7d9acf5a4511529e47ea99ad2682db8fd167b8b
Lijun <lijunhw@users.noreply.github.com>
2016-09-13 23:18:53 -0700

    Change BLE ADV flag to general discoverable
    And also shorten the ADV interval

--- Arduino/nRF24_BLE_advertizer_demo/nRF24_BLE_advertizer_demo.ino
+++ Arduino/nRF24_BLE_advertizer_demo/nRF24_BLE_advertizer_demo.ino
@@ -1,16 +1,16 @@
 // Inspired by http://dmitry.gr/index.php?r=05.Projects&proj=11.%20Bluetooth%20LE%20fakery
 
 #include "SPI.h"  // SPI in Arduino Uno/Nano: MOSI pin 11, MISO pin 12, SCK pin 13
-#define PIN_CE	10 // chip enable
-#define PIN_CSN	9   // chip select (for SPI)
+#define PIN_CE  10 // chip enable
+#define PIN_CSN 9   // chip select (for SPI)
 
 // The MAC address of BLE advertizer -- just make one up
-#define MY_MAC_0	0x11
-#define MY_MAC_1	0x22
-#define MY_MAC_2	0x33
-#define MY_MAC_3	0x44
-#define MY_MAC_4	0x55
-#define MY_MAC_5	0x66
+#define MY_MAC_0  0x11
+#define MY_MAC_1  0x22
+#define MY_MAC_2  0x33
+#define MY_MAC_3  0x44
+#define MY_MAC_4  0x55
+#define MY_MAC_5  0x66
 
 uint8_t buf[32];   
 static const uint8_t chRf[] = {2, 26,80};
@@ -21,7 +21,7 @@ void btLeCrc(const uint8_t* data, uint8_t len, uint8_t* dst){
 // implementing CRC with LFSR
     uint8_t v, t, d;
     
-    while(len--){	
+    while(len--){ 
         d = *data++;
         for(v = 0; v < 8; v++, d >>= 1){
             t = dst[0] >> 7;
@@ -35,7 +35,7 @@ void btLeCrc(const uint8_t* data, uint8_t len, uint8_t* dst){
               dst[2] ^= 0x5B;
               dst[1] ^= 0x06;
             }
-        }	
+        } 
     }
 }
 
@@ -70,7 +70,7 @@ void btLeWhiten(uint8_t* data, uint8_t len, uint8_t whitenCoeff){
 
 static inline uint8_t btLeWhitenStart(uint8_t chan){
 //the value we actually use is what BT'd use left shifted one...makes our life easier
-    return swapbits(chan) | 2;	
+    return swapbits(chan) | 2;  
 }
 
 void btLePacketEncode(uint8_t* packet, uint8_t len, uint8_t chan){
@@ -83,7 +83,7 @@ void btLePacketEncode(uint8_t* packet, uint8_t len, uint8_t chan){
     btLeWhiten(packet, len, btLeWhitenStart(chan));
     for(i = 0; i < len; i++) 
         packet[i] = swapbits(packet[i]); // the byte order of the packet should be reversed as well
-	
+  
 }
 
 uint8_t spi_byte(uint8_t byte){
@@ -112,7 +112,7 @@ void nrf_manybytes(uint8_t* data, uint8_t len){
     digitalWrite(PIN_CSN, LOW); 
         do{
             spi_byte(*data++);
-	}while(--len);
+  }while(--len);
         digitalWrite(PIN_CSN, HIGH); 
 }
 
@@ -131,17 +131,17 @@ void setup() {
     SPI.setBitOrder(MSBFIRST);
     
     // Now initialize nRF24L01+, setting general parameters
-    nrf_cmd(0x20, 0x12);	//on, no crc, int on RX/TX done
-    nrf_cmd(0x21, 0x00);	//no auto-acknowledge
-    nrf_cmd(0x22, 0x00);	//no RX
-    nrf_cmd(0x23, 0x02);	//4-byte address 
-    nrf_cmd(0x24, 0x00);	//no auto-retransmit
-    nrf_cmd(0x26, 0x06);	//1MBps at 0dBm
-    nrf_cmd(0x27, 0x3E);	//clear various flags
-    nrf_cmd(0x3C, 0x00);	//no dynamic payloads
-    nrf_cmd(0x3D, 0x00);	//no features
-    nrf_cmd(0x31, 32);	        //always RX 32 bytes
-    nrf_cmd(0x22, 0x01);	//RX on pipe 0
+    nrf_cmd(0x20, 0x12);  //on, no crc, int on RX/TX done
+    nrf_cmd(0x21, 0x00);  //no auto-acknowledge
+    nrf_cmd(0x22, 0x00);  //no RX
+    nrf_cmd(0x23, 0x02);  //4-byte address 
+    nrf_cmd(0x24, 0x00);  //no auto-retransmit
+    nrf_cmd(0x26, 0x06);  //1MBps at 0dBm
+    nrf_cmd(0x27, 0x3E);  //clear various flags
+    nrf_cmd(0x3C, 0x00);  //no dynamic payloads
+    nrf_cmd(0x3D, 0x00);  //no features
+    nrf_cmd(0x31, 32);          //always RX 32 bytes
+    nrf_cmd(0x22, 0x01);  //RX on pipe 0
     
     // Set access addresses (TX address in nRF24L01) to BLE advertising 0x8E89BED6
     // Remember that both bit and byte orders are reversed for BLE packet format
@@ -158,11 +158,11 @@ void setup() {
 void loop() {
     // Channel hopping
     for (ch=0; ch<sizeof(chRf); ch++)
-    {	
+    { 
         uint8_t i, L=0;
         
-        buf[L++] = 0x42;	//PDU type, given address is random; 0x42 for Android and 0x40 for iPhone
-        buf[L++] = 16+4; // length of payload
+        buf[L++] = 0x42;  //PDU type, given address is random; 0x42 for Android and 0x40 for iPhone
+        buf[L++] = 16+5; // length of payload
             
         buf[L++] = MY_MAC_0;
         buf[L++] = MY_MAC_1;
@@ -171,10 +171,10 @@ void loop() {
         buf[L++] = MY_MAC_4;
         buf[L++] = MY_MAC_5;
             
-        buf[L++] = 2;		//flags (LE-only, limited discovery mode)
+        buf[L++] = 2;   //flags (LE-only, general discoverable mode)
         buf[L++] = 0x01;
-        buf[L++] = 0x05;
-    		
+        buf[L++] = 0x06;
+        
         buf[L++] = 6;   // length of the name, including type byte
         buf[L++] = 0x08;
         buf[L++] = 'n';
@@ -183,17 +183,18 @@ void loop() {
         buf[L++] = '2';
         buf[L++] = '4';
             
-        buf[L++] = 3;   // length of custom data, including type byte
+        buf[L++] = 4;   // length of custom data, including type byte
         buf[L++] = 0xff;   
         buf[L++] = 0x01;
-        buf[L++] = 0x02;  // some test data
+        buf[L++] = 0x02;  
+        buf[L++] = 0x03;// some test data
             
-        buf[L++] = 0x55;	//CRC start value: 0x555555
+        buf[L++] = 0x55;  //CRC start value: 0x555555
         buf[L++] = 0x55;
         buf[L++] = 0x55;
 
         nrf_cmd(0x25, chRf[ch]);
-        nrf_cmd(0x27, 0x6E);	// Clear flags
+        nrf_cmd(0x27, 0x6E);  // Clear flags
             
         btLePacketEncode(buf, L, chLe[ch]);
         nrf_simplebyte(0xE2); //Clear RX Fifo
@@ -204,11 +205,11 @@ void loop() {
         for(i = 0 ; i < L ; i++) spi_byte(buf[i]);
         digitalWrite(PIN_CSN, HIGH); 
         
-        nrf_cmd(0x20, 0x12);	// TX on
+        nrf_cmd(0x20, 0x12);  // TX on
         digitalWrite(PIN_CE, HIGH); // Enable Chip
         delay(2);        // 
         digitalWrite(PIN_CE, LOW);   // (in preparation of switching to RX quickly)
     }
-    delay(500);    // Broadcasting interval
+    delay(200);    // Broadcasting interval
 }
 
